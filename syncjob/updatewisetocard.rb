b5d6ccb1ae12dda6cd563bfafb0629d224b8f756
Bernd Löffeld <Bernd.Loeffeld@magicinternet.de>
2013-06-03 17:10:46 +0200

    pushed the key ... key is deleted from skript and from scrumwise now

--- syncjob/updatewisetocard.rb
+++ syncjob/updatewisetocard.rb
@@ -11,8 +11,6 @@ log.info('Synchronize the scrumwise project into our CouchDB')
 apicl = HTTPClient.new
 couchcl = HTTPClient.new
 
-user = 'bernd.loeffeld@magicinternet.de'
-password = '4967BAF4BC5AE91E6139230742E121BD5CD64E642F3C2111C67BA91CECB27C3B'
 apicl.set_auth(nil, user, password)
 
 orig_data = apicl.get("https://api.scrumwise.com/service/api/v1/getData", {"includeProperties" => "Project.backlogItems"}).body
